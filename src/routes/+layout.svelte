2a57bf54c5d92542386e978df8e8018e8ae57e83
DevWedeloper <vicnathangabrielle@gmail.com>
2025-11-06 12:33:02 +0800

    feat: create base layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script lang='ts'>
+  import { page } from '$app/stores'
   import favicon from '$lib/assets/favicon.svg'
+  import Navbar from '$lib/components/Navbar.svelte'
   import '../app.css'
 
   const { children } = $props()
@@ -7,6 +9,16 @@
 
 <svelte:head>
   <link rel='icon' href={favicon} />
+
+  <link rel='canonical' href='{$page.url.origin}{$page.url.pathname}' />
+
+  <title>Pokédex</title>
+  <meta name='title' content='Pokédex' />
+  <meta name='description' content='A modern Pokédex built with SvelteKit.' />
+  <meta name='author' content='DevWedeloper ' />
+
 </svelte:head>
+
+<Navbar />
 
 {@render children()}
