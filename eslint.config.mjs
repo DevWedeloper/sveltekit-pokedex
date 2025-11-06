a047a646954e690d13301b2caaa5f2cfd8e2b6ee
DevWedeloper <vicnathangabrielle@gmail.com>
2025-11-06 12:15:04 +0800

    chore: configure eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import antfu from '@antfu/eslint-config'
+import eslintPluginBetterTailwindcss from 'eslint-plugin-better-tailwindcss'
+
+export default antfu({
+  formatters: true,
+  svelte: true,
+}, {
+  plugins: {
+    'better-tailwindcss': eslintPluginBetterTailwindcss,
+  },
+  rules: {
+    ...eslintPluginBetterTailwindcss.configs['recommended-warn'].rules,
+    ...eslintPluginBetterTailwindcss.configs['recommended-error'].rules,
+    'better-tailwindcss/enforce-consistent-line-wrapping': ['off'],
+    'better-tailwindcss/no-unregistered-classes': ['off'],
+  },
+})
