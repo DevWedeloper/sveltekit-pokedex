006376395bef2b3fca98f0a73350693bc49aee1c
DevWedeloper <vicnathangabrielle@gmail.com>
2025-11-08 13:16:48 +0800

    refactor: rename /pokedex -> /pokemon

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -2,7 +2,7 @@
   <div class='mx-auto flex max-w-6xl items-center justify-between px-6 py-4'>
     <a href='/' class='text-primary text-xl font-bold transition hover:opacity-90'>Pokédex</a>
     <div class='text-foreground flex space-x-6 font-medium'>
-      <a href='/pokedex' class='hover:text-primary transition'>Pokédex</a>
+      <a href='/pokemon' class='hover:text-primary transition'>Pokédex</a>
     </div>
   </div>
 </nav>
