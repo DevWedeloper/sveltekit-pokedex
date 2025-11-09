935b5ea197accc5f41d314d07a403f68f69fc46c
DevWedeloper <vicnathangabrielle@gmail.com>
2025-11-09 09:11:17 +0800

    chore: use ts path aliasing

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,4 @@
+import path from 'node:path'
 import adapter from '@sveltejs/adapter-auto'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
@@ -12,6 +13,9 @@ const config = {
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
     adapter: adapter(),
+    alias: {
+      '@': path.resolve('./src'),
+    },
   },
 }
 
