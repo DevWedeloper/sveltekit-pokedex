44db157f50e96dd155c78d3292f19e231a544ef7
DevWedeloper <vicnathangabrielle@gmail.com>
2025-11-11 16:32:03 +0800

    feat: create constants

---
+++ src/lib/constants/statColors.ts
@@ -0,0 +1,8 @@
+export const statColors: Record<string, string> = {
+  'hp': '#D62828',
+  'attack': '#C57B00',
+  'defense': '#B59F3B',
+  'special-attack': '#2A6BF2',
+  'special-defense': '#3B8C59',
+  'speed': '#C44569',
+}
