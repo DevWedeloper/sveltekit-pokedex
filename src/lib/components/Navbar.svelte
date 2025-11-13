a1c4e13d39b375e0063fdd27d393c3335328b7d7
DevWedeloper <vicnathangabrielle@gmail.com>
2025-11-13 14:57:50 +0800

    style: pokedex -> pokemon

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -2,7 +2,7 @@
   <div class='mx-auto flex max-w-6xl items-center justify-between px-6 py-4'>
     <a href='/' class='text-primary text-xl font-bold transition hover:opacity-90'>Pokédex</a>
     <div class='text-foreground flex space-x-6 font-medium'>
-      <a href='/pokemon' class='hover:text-primary transition'>Pokédex</a>
+      <a href='/pokemon' class='hover:text-primary transition'>Pokemon</a>
     </div>
   </div>
 </nav>
