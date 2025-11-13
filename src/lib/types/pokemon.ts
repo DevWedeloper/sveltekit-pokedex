895f2e9bfb97ab0a263e564bc88c5f7a8b59af8e
DevWedeloper <vicnathangabrielle@gmail.com>
2025-11-13 14:13:58 +0800

    feat: create type PokemonDetails

--- src/lib/types/pokemon.ts
+++ src/lib/types/pokemon.ts
@@ -1,3 +1,4 @@
-import type { PokemonListQuery } from '@/gql/graphql'
+import type { PokemonDetailsByIdQuery, PokemonListQuery } from '@/gql/graphql'
 
 export type Pokemon = PokemonListQuery['pokemon'][0] | undefined | null
+export type PokemonDetails = PokemonDetailsByIdQuery['pokemon'][0]
