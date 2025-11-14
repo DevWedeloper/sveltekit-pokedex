b48f1b8b43f4ad478a8b5eff862fadcd2ced8f4c
DevWedeloper <vicnathangabrielle@gmail.com>
2025-11-14 15:58:18 +0800

    feat: show ui on pokemon details loading

--- src/routes/pokemon/[id]/+page.svelte
+++ src/routes/pokemon/[id]/+page.svelte
@@ -4,10 +4,12 @@
   import * as Dialog from '$lib/components/ui/dialog/index.js'
   import { getPokemonDetailsQuery } from '$lib/data-access/getPokemonDetails'
   import PokemonDetailsCard from './ui/PokemonDetailsCard.svelte'
+  import PokemonDetailsSkeleton from './ui/PokemonDetailsSkeleton.svelte'
 
   const { data }: { data: PageData } = $props()
 
   const pokemonDetails = $derived(getPokemonDetailsQuery(data.id))
+  const pokemonDetailsLoading = $derived(pokemonDetails.isFetching)
   const pokemonData = $derived(pokemonDetails.data)
 
   let innerWidth = $state(typeof window !== 'undefined' ? window.innerWidth : 0)
@@ -18,22 +20,28 @@
       goto('/pokemon')
     }
   })
+
+  $inspect(pokemonDetailsLoading)
 </script>
 
 <svelte:window bind:innerWidth />
 
-{#if pokemonData}
-  <PokemonDetailsCard pokemon={pokemonData} />
+{#if !pokemonDetailsLoading}
+  {#if pokemonData}
+    <PokemonDetailsCard pokemon={pokemonData} />
+  {/if}
 {:else}
-  <p>Loading...</p>
+  <PokemonDetailsSkeleton />
 {/if}
 
 <Dialog.Root bind:open={isOpen}>
   <Dialog.Content>
-    {#if pokemonData}
-      <PokemonDetailsCard pokemon={pokemonData} />
+    {#if !pokemonDetailsLoading}
+      {#if pokemonData}
+        <PokemonDetailsCard pokemon={pokemonData} />
+      {/if}
     {:else}
-      <p>Loading...</p>
+      <PokemonDetailsSkeleton />
     {/if}
   </Dialog.Content>
 </Dialog.Root>
---
+++ src/routes/pokemon/[id]/ui/PokemonDetailsSkeleton.svelte
@@ -0,0 +1,51 @@
+<script lang='ts'>
+  import { Skeleton } from '$lib/components/ui/skeleton'
+
+</script>
+
+<div class='flex justify-center'>
+  <div class='flex max-w-80 flex-col items-center gap-4 rounded-2xl p-4 text-center'>
+    <!-- Pokémon Image Skeleton -->
+    <Skeleton class='h-40 w-40 rounded-full' />
+
+    <!-- ID and Name -->
+    <div class='flex flex-col items-center gap-2'>
+      <Skeleton class='h-4 w-12' /> <!-- ID -->
+      <Skeleton class='h-8 w-32' /> <!-- Name -->
+
+      <!-- Type Badges -->
+      <div class='flex justify-center gap-2'>
+        <Skeleton class='h-6 w-16 rounded-md' />
+        <Skeleton class='h-6 w-16 rounded-md' />
+      </div>
+
+      <!-- Pokedex Entry -->
+      <div class='mt-4 space-y-2'>
+        <Skeleton class='h-5 w-20' /> <!-- "Pokedex Entry" label -->
+        <Skeleton class='h-4 w-60' />
+        <Skeleton class='h-4 w-56' />
+        <Skeleton class='h-4 w-64' />
+      </div>
+
+      <!-- Height & Weight -->
+      <div class='mt-4 flex w-full justify-between gap-6'>
+        <Skeleton class='h-8 w-20 rounded-lg' />
+        <Skeleton class='h-8 w-20 rounded-lg' />
+      </div>
+
+      <!-- Abilities -->
+      <div class='mt-4 flex flex-wrap justify-center gap-4'>
+        <Skeleton class='h-6 w-24 rounded-lg' />
+        <Skeleton class='h-6 w-24 rounded-lg' />
+      </div>
+
+      <!-- Stats -->
+      <div class='mt-4 flex flex-wrap justify-center gap-4'>
+        {#each Array.from({ length: 7 }) as _}
+          <Skeleton class='h-8 w-8 rounded-full' />
+        {/each}
+      </div>
+
+    </div>
+  </div>
+</div>
