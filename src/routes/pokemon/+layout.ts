2805f908b83326cf411e5ccd7628c8480e43a8c5
DevWedeloper <vicnathangabrielle@gmail.com>
2025-11-14 13:57:09 +0800

    fix: flicker on load

--- src/routes/pokemon/+layout.ts
+++ src/routes/pokemon/+layout.ts
@@ -1,5 +1,4 @@
 import type { LayoutLoad } from './$types'
-import { browser } from '$app/environment'
 import { fetchFilters, FILTERS_QUERY_KEY } from '$lib/data-access/getFilters'
 import { fetchPokemons, POKEMONS_QUERY_KEY } from '$lib/data-access/getPokemons'
 import { LIMIT } from '@/lib/constants/paginations'
@@ -10,26 +9,24 @@ export const load: LayoutLoad = async ({ parent, fetch, url }) => {
 
   const filter = { filterType, filterValue }
 
-  if (!browser) {
-    const limit = LIMIT
-    const { queryClient } = await parent()
+  const limit = LIMIT
+  const { queryClient } = await parent()
 
-    await queryClient.prefetchInfiniteQuery({
-      queryKey: POKEMONS_QUERY_KEY(filter),
-      queryFn: ({ pageParam }) => fetchPokemons({ filter, offset: pageParam, limit, fetch }),
-      initialPageParam: 0,
-      getNextPageParam: (lastPage: any, allPages: any) => {
-        if (lastPage.length < limit)
-          return undefined
-        return allPages.length * limit
-      },
-    })
+  await queryClient.prefetchInfiniteQuery({
+    queryKey: POKEMONS_QUERY_KEY(filter),
+    queryFn: ({ pageParam }) => fetchPokemons({ filter, offset: pageParam, limit, fetch }),
+    initialPageParam: 0,
+    getNextPageParam: (lastPage: any, allPages: any) => {
+      if (lastPage.length < limit)
+        return undefined
+      return allPages.length * limit
+    },
+  })
 
-    await queryClient.prefetchQuery({
-      queryKey: FILTERS_QUERY_KEY(),
-      queryFn: () => fetchFilters({ fetch }),
-    })
-  }
+  await queryClient.prefetchQuery({
+    queryKey: FILTERS_QUERY_KEY(),
+    queryFn: () => fetchFilters({ fetch }),
+  })
 
-  return ({ filter })
+  return { filter }
 }
