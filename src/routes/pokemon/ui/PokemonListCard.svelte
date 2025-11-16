4bf5db5a2f3d163be91b3962ed48cde81910304c
DevWedeloper <vicnathangabrielle@gmail.com>
2025-11-16 22:36:48 +0800

    style: change pokemon list card

--- src/routes/pokemon/ui/PokemonListCard.svelte
+++ src/routes/pokemon/ui/PokemonListCard.svelte
@@ -17,25 +17,27 @@
   const pokemonUrl = $derived(`/pokemon/${pokemon.id}${currentQuery}`)
 </script>
 
-<a
-  href={pokemonUrl}
-  class='group text-card-foreground @container-[size] relative aspect-square overflow-hidden rounded-xl'
->
-  {#each pokemon.pokemontypes as type, index}
-    <div
-      class={cn('absolute z-0 size-[80cqw] rounded-full blur-[50cqw] filter', circlePositions[index])}
-      style={`background: ${typeColors[type.type?.name ?? '#A8A8A8']};`}
-    ></div>
-  {/each}
+<div class='flex flex-col gap-2'>
+  <a
+    href={pokemonUrl}
+    class='group text-card-foreground @container-[size] relative aspect-square w-full overflow-hidden rounded-xl'
+  >
+    {#each pokemon.pokemontypes as type, index}
+      <div
+        class={cn('absolute size-[80cqw] rounded-full blur-[50cqw] filter', circlePositions[index])}
+        style={`background: ${typeColors[type.type?.name ?? '#A8A8A8']};`}
+      ></div>
+    {/each}
 
-  <div class='relative z-20 flex h-full flex-col items-center justify-center p-2 text-center'>
-    <img
-      src={pokemon.pokemonsprites[0]?.sprites || '/pokeball-silhouette.svg'}
-      alt={pokemon.name}
-      class='size-[80cqw] transition-transform select-none group-hover:scale-110 md:size-[60cqw]'
-      loading='lazy'
-    />
-    <p class='text-sm font-medium capitalize'>{pokemon.name}</p>
-    <p class='text-muted-foreground text-xs'>#{pokemon.id}</p>
-  </div>
-</a>
+    <div class='relative flex h-full flex-col items-center justify-center p-2 text-center'>
+      <img
+        src={pokemon.pokemonsprites[0]?.sprites || '/pokeball-silhouette.svg'}
+        alt={pokemon.name}
+        class='size-[80cqw] transition-transform select-none group-hover:scale-110 md:size-[60cqw]'
+        loading='lazy'
+      />
+    </div>
+  </a>
+
+  <p class='text-center text-sm font-medium capitalize'>{pokemon.name} <span class='text-muted-foreground'>#{pokemon.id}</span></p>
+</div>
