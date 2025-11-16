fe5af762f1e71893b2bea508529d720c80fc8f93
DevWedeloper <vicnathangabrielle@gmail.com>
2025-11-16 22:10:58 +0800

    refactor: remove nullish

--- src/lib/types/pokemon.ts
+++ src/lib/types/pokemon.ts
@@ -1,4 +1,4 @@
 import type { PokemonDetailsByIdQuery, PokemonListQuery } from '$lib/gql/graphql'
 
-export type Pokemon = PokemonListQuery['pokemon'][0] | undefined | null
+export type Pokemon = PokemonListQuery['pokemon'][0]
 export type PokemonDetails = PokemonDetailsByIdQuery['pokemon'][0]
--- src/routes/pokemon/ui/PokemonListCard.svelte
+++ src/routes/pokemon/ui/PokemonListCard.svelte
@@ -14,30 +14,28 @@
   ]
 
   const currentQuery = $derived(page.url.search)
-  const pokemonUrl = $derived(pokemon ? `/pokemon/${pokemon.id}${currentQuery}` : '')
+  const pokemonUrl = $derived(`/pokemon/${pokemon.id}${currentQuery}`)
 </script>
 
-{#if pokemon}
-  <a
-    href={pokemonUrl}
-    class='group text-card-foreground @container-[size] relative aspect-square overflow-hidden rounded-xl'
-  >
-    {#each pokemon.pokemontypes as type, index}
-      <div
-        class={cn('absolute z-0 size-[80cqw] rounded-full blur-[50cqw] filter', circlePositions[index])}
-        style={`background: ${typeColors[type.type?.name ?? '#A8A8A8']};`}
-      ></div>
-    {/each}
+<a
+  href={pokemonUrl}
+  class='group text-card-foreground @container-[size] relative aspect-square overflow-hidden rounded-xl'
+>
+  {#each pokemon.pokemontypes as type, index}
+    <div
+      class={cn('absolute z-0 size-[80cqw] rounded-full blur-[50cqw] filter', circlePositions[index])}
+      style={`background: ${typeColors[type.type?.name ?? '#A8A8A8']};`}
+    ></div>
+  {/each}
 
-    <div class='relative z-20 flex h-full flex-col items-center justify-center p-2 text-center'>
-      <img
-        src={pokemon.pokemonsprites[0]?.sprites || '/pokeball-silhouette.svg'}
-        alt={pokemon.name}
-        class='size-[80cqw] transition-transform select-none group-hover:scale-110 md:size-[60cqw]'
-        loading='lazy'
-      />
-      <p class='text-sm font-medium capitalize'>{pokemon.name}</p>
-      <p class='text-muted-foreground text-xs'>#{pokemon.id}</p>
-    </div>
-  </a>
-{/if}
+  <div class='relative z-20 flex h-full flex-col items-center justify-center p-2 text-center'>
+    <img
+      src={pokemon.pokemonsprites[0]?.sprites || '/pokeball-silhouette.svg'}
+      alt={pokemon.name}
+      class='size-[80cqw] transition-transform select-none group-hover:scale-110 md:size-[60cqw]'
+      loading='lazy'
+    />
+    <p class='text-sm font-medium capitalize'>{pokemon.name}</p>
+    <p class='text-muted-foreground text-xs'>#{pokemon.id}</p>
+  </div>
+</a>
