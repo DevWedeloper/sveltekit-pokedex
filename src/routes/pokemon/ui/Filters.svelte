ba2df5fccbbbeeeb47419eaf6c8dc2b2e5138714
DevWedeloper <vicnathangabrielle@gmail.com>
2025-11-16 21:45:05 +0800

    refactor: use snippet

--- src/routes/pokemon/ui/Filters.svelte
+++ src/routes/pokemon/ui/Filters.svelte
@@ -17,6 +17,19 @@
   const capitalize = (s: string) => s.charAt(0).toUpperCase() + s.slice(1)
 </script>
 
+{#snippet filterGroup(title: string, items: { name: string }[], filterType: string)}
+  <h3 class='mt-3 mb-2 font-semibold'>{title}</h3>
+  <ScrollArea orientation='horizontal' class='w-full'>
+    <div class='flex gap-2 pb-2'>
+      {#each items as item}
+        <Button variant='outline' onclick={() => updateFilter(filterType, item.name)}>
+          {capitalize(item.name)}
+        </Button>
+      {/each}
+    </div>
+  </ScrollArea>
+{/snippet}
+
 <div class='mb-6'>
   <Button variant='destructive' onclick={resetFilters}>
     Reset
@@ -25,25 +38,6 @@
   <h3 class='mt-3 mb-2 font-semibold'>Search</h3>
   <Input bind:value={search} {oninput} type='text' placeholder='Search...' />
 
-  <h3 class='mt-3 mb-2 font-semibold'>Types</h3>
-  <ScrollArea orientation='horizontal' class='w-full'>
-    <div class='flex gap-2 pb-2'>
-      {#each filters.type as type}
-        <Button variant='outline' onclick={() => updateFilter('type', type.name)}>
-          {capitalize(type.name)}
-        </Button>
-      {/each}
-    </div>
-  </ScrollArea>
-
-  <h3 class='mt-3 mb-2 font-semibold'>Regions</h3>
-  <ScrollArea orientation='horizontal' class='w-full'>
-    <div class='flex gap-2 pb-2'>
-      {#each filters.region as region}
-        <Button variant='outline' onclick={() => updateFilter('region', region.name)}>
-          {capitalize(region.name)}
-        </Button>
-      {/each}
-    </div>
-  </ScrollArea>
+  {@render filterGroup('Types', filters.type, 'type')}
+  {@render filterGroup('Regions', filters.region, 'region')}
 </div>
